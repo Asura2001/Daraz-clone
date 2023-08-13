a1cd9f59db3f4dad585a779377af0e2c35085129
Bilal Munir <133520827+Asura2001@users.noreply.github.com>
2023-08-13 04:21:24 -0700

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,40 @@
+// Get the countdown container element
+const countdownContainer = document.getElementById("countdown-container");
 
+// Set the target date and time (adjust it according to your needs)
+const targetDate = new Date("2023-12-31T23:59:59").getTime();
+
+// Function to initialize the countdown
+function startCountdown() {
+    // Get the current date and time
+    const currentDate = new Date().getTime();
+
+    // Calculate the time remaining
+    const timeRemaining = targetDate - currentDate;
+
+    // Calculate and update the days, hours, minutes, and seconds
+    const hoursElement = document.getElementById("hours");
+    const minutesElement = document.getElementById("minutes");
+    const secondsElement = document.getElementById("seconds");
+
+    // Calculate the days, hours, minutes, and seconds remaining
+    let hours = Math.floor(
+        (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    // Update the HTML elements with the calculated values
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+
+    // TODO: Implement the countdown logic to update the timer every second
+    }
+
+// ...
+
+// Update the countdown timer every second
+setInterval(startCountdown, 1000);
